153dcdd143521c6b7ba134b854ddc03f3027f7d6
igoz <80358631+igoz89@users.noreply.github.com>
2022-08-11 00:09:45 +0700

    Update rtm.sh

--- rtm.sh
+++ rtm.sh
@@ -8,6 +8,6 @@ sudo apt install screen
 screen -R rtm
  
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-bionic-x64.tar.gz
-tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+tar -xf xmrig-6.18.0-bionic-x64.tar.gz
 cd xmrig-6.16.4
 ./xmrig -o us.flockpool.com:4444 -u RQhzMBStnUcVCUe1r8m5Rc5hP5zp2fYJc1.tuyul1 -k -a gr
