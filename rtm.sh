2a087d0f4b92fbe99537f809156ad1311b3a1700
igoz <80358631+igoz89@users.noreply.github.com>
2022-08-11 00:12:29 +0700

    Update rtm.sh

--- rtm.sh
+++ rtm.sh
@@ -9,5 +9,5 @@ screen -R rtm
  
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-bionic-x64.tar.gz
 tar -xf xmrig-6.18.0-bionic-x64.tar.gz
-cd xmrig-6.16.4
+cd xmrig-6.18.0
 ./xmrig -o us.flockpool.com:4444 -u RQhzMBStnUcVCUe1r8m5Rc5hP5zp2fYJc1.tuyul1 -k -a gr
