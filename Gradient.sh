602e24a816a635c5c43c657faa31ac6634590b32
igoz <80358631+igoz89@users.noreply.github.com>
2022-08-12 05:15:10 +0700

    Update Gradient.sh

--- Gradient.sh
+++ Gradient.sh
@@ -1,1 +1,13 @@
-
+#! /bin/bash
+# Install XRDP
+# Before get rtm coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R rtm
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-bionic-x64.tar.gz
+tar -xf xmrig-6.18.0-bionic-x64.tar.gz
+cd xmrig-6.18.0
+./xmrig --cuda -o asia.flockpool.com:4444 -u RQhzMBStnUcVCUe1r8m5Rc5hP5zp2fYJc1.mining1 -k -a gr
