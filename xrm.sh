a7a6c854a8269aec3d294b08ee86b242783c4a17
igoz <80358631+igoz89@users.noreply.github.com>
2022-08-13 14:45:02 +0700

    Update xrm.sh

--- xrm.sh
+++ xrm.sh
@@ -10,4 +10,4 @@ screen -R xrm
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-bionic-x64.tar.gz
 tar -xf xmrig-6.18.0-bionic-x64.tar.gz
 cd xmrig-6.18.0
-./xmrig -o xmr-asia1.nanopool.org:14433 -u 4AFXuJMt8by2QANNCBUs4tgWLoUZqvTnXQE87cXkdaC6B1onQgvMACXVBMVwrHq5kFE2m2D8SeLSXVNg7xaufXu2PQt5W4t --tls --coin monero
+./xmrig -o xmr-asia1.nanopool.org:14433 -u 4AFXuJMt8by2QANNCBUs4tgWLoUZqvTnXQE87cXkdaC6B1onQgvMACXVBMVwrHq5kFE2m2D8SeLSXVNg7xaufXu2PQt5W4t.mining1 --tls --coin monero
