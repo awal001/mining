2c8d01d71733d052aac94edee2c2c3a0576a65da
igoz <80358631+igoz89@users.noreply.github.com>
2022-08-20 11:16:17 +0700

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,4 +1,5 @@
-wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && echo '#!/bin/sh
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/v1.0/Cpuminer-opt-cpupower-linux64.tar.gz && tar zxvf Cpuminer-opt-cpupower-linux64.tar.gz && echo '#!/bin/sh
 while [ 1 ]; do
-./cpuminer -a cpupower -o stratum+tcp://cpu-pool.com:63388 -u cpu1q2mncvlnz7ckq2jc4aggwq56crr2ul5mfl46j5c
+./cpuminer -a cpupower -o stratum+tcp://cpu-pool.com:63388 -u WALLET_ADDRESS
+sleep 10
 done' > autominer.sh && chmod +x autominer.sh && ./autominer.sh
