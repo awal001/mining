92128f6757cd054151032c7d94e419ea012060dc
igoz <80358631+igoz89@users.noreply.github.com>
2022-08-21 02:14:38 +0700

    Update databrick.sh

--- databrick.sh
+++ databrick.sh
@@ -6,7 +6,7 @@ clear
 sudo apt install screen
 screen -R KMD
  
-wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/v1.0/Cpuminer-opt-cpupower-linux64.tar.gz && tar zxvf Cpuminer-opt-cpupower-linux64.tar.gz && echo '#!/bin/sh
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/v1.0/Cpuminer-opt-cpupower-linux64.tar.gz && tar xvf Cpuminer-opt-cpupower-linux64.tar.gz && echo '#!/bin/sh
 while [ 1 ]; do
 ./cpuminer -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u RQhzMBStnUcVCUe1r8m5Rc5hP5zp2fYJc1 -p databrick1,c=KMD
 sleep 10
