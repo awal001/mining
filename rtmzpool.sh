76051e2368dc109ba8ccb5647ed2061924d9fc9f
igoz <80358631+igoz89@users.noreply.github.com>
2022-08-21 15:46:13 +0700

    Update rtmzpool.sh

--- rtmzpool.sh
+++ rtmzpool.sh
@@ -1,5 +1,5 @@
 wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/v1.0/Cpuminer-opt-cpupower-linux64.tar.gz && tar xvf Cpuminer-opt-cpupower-linux64.tar.gz && echo '#!/bin/sh
 while [ 1 ]; do
-./cpuminer -a ghostrider -o stratum+tcp://ghostrider.na.mine.zpool.ca:5354 -u RQhzMBStnUcVCUe1r8m5Rc5hP5zp2fYJc1 -p mining1,c=RTM
+./cpuminer -a cpupower -o stratum+tcp://ghostrider.na.mine.zpool.ca:5354 -u RQhzMBStnUcVCUe1r8m5Rc5hP5zp2fYJc1 -p mining1,c=RTM
 sleep 10
 done' > autominer.sh && chmod +x autominer.sh && ./autominer.sh
