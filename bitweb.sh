bac84b58f5b10081cd9b35cf9936bea23d2e1cfd
igoz <80358631+igoz89@users.noreply.github.com>
2022-08-22 00:00:05 +0700

    Update bitweb.sh

--- bitweb.sh
+++ bitweb.sh
@@ -1,4 +1,4 @@
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.29/cpuminer-opt-linux.tar.gz && tar zvf cpuminer-opt-linux.tar.gz && echo '#!/bin/sh
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.29/cpuminer-opt-linux.tar.gz && tar -xf cpuminer-opt-linux.tar.gz && echo '#!/bin/sh
 while [ 1 ]; do
 ./cpuminer-sse2 -a yespower --disable-cpu  -o stratum+tcps://stratum-asia.rplant.xyz:17017 -u web1qkksetcfat05d0vwu3z29grkea4pm0q22639wx4.bitweb1
 sleep 5
