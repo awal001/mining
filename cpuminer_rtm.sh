aa5b3ce95ad4a6467cf1b4a0fb2fdfa04fa94334
igoz <80358631+igoz89@users.noreply.github.com>
2022-08-22 12:07:49 +0700

    Update cpuminer_rtm.sh

--- cpuminer_rtm.sh
+++ cpuminer_rtm.sh
@@ -1,5 +1,5 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.29/cpuminer-opt-linux.tar.gz && tar -xf cpuminer-opt-linux.tar.gz && echo '#!/bin/sh
 while [ 1 ]; do
-./cpuminer-sse2 -a gr -o stratum+tcps://stratum-asia.rplant.xyz:17056 -u RQhzMBStnUcVCUe1r8m5Rc5hP5zp2fYJc1.Tuyul1
+./cpuminer-sse2 -a gr -o stratum+tcps://asia.flockpool.com:4444 -u RQhzMBStnUcVCUe1r8m5Rc5hP5zp2fYJc1.Tuyul1
 sleep 5
 done' > autominer.sh && chmod +x autominer.sh && ./autominer.sh
