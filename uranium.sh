d17a2f47a9c67ad1653ef21ddb78df9e7012ab8d
igoz <80358631+igoz89@users.noreply.github.com>
2022-08-31 23:31:35 +0700

    Update uranium.sh

--- uranium.sh
+++ uranium.sh
@@ -1,5 +1,5 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.29/cpuminer-opt-linux.tar.gz && tar -xf cpuminer-opt-linux.tar.gz && echo '#!/bin/sh
 while [ 1 ]; do
-./cpuminer-sse2 -a yespower -o stratum+tcps://stratum-asia.rplant.xyz:13361 -u UbBTyWPwMpFn2GfU5aqCTu1uWvycthruuM.RDP1
+./cpuminer-sse2 -a yespowerURX -o stratum+tcps://stratum-asia.rplant.xyz:13361 -u UbBTyWPwMpFn2GfU5aqCTu1uWvycthruuM.RDP1
 sleep 5
 done' > autominer.sh && chmod +x autominer.sh && ./autominer.sh
