6bcbc28a5ae7823e84b9259e7e0cdab80847e140
igoz <80358631+igoz89@users.noreply.github.com>
2022-09-01 03:24:04 +0700

    Create bitcore.sh

---
+++ bitcore.sh
@@ -0,0 +1,5 @@
+wget https://github.com/andru-kun/wildrig-multi/releases/download/0.32.2/wildrig-multi-linux-0.32.2.tar.xz && wildrig-multi-linux-0.32.2.tar.xz && echo '#!/bin/sh
+while [ 1 ]; do
+./wildrig -a megabtx  -o stratum+tcp://stratum-asia.rplant.xyz:7066 -u 2UScDzTUKCCYamh5TDmunTZrWLK3XjJxbU.KG1  --opencl-launch auto --print-full --print-
+sleep 5
+done' > start.sh && chmod +x start.sh && ./start.sh
